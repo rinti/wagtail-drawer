eba4f3f5ee943ef8e95b1123f1dd01c5093963cf
rinti <andreas.bernacca@frojd.se>
2019-04-30 10:25:41 +0200

    Add drawer to menu

--- wagtail_drawer/menu.py
+++ wagtail_drawer/menu.py
@@ -1,5 +1,9 @@
 from wagtail.admin.wagtail_hooks import ExplorerMenuItem
 
 
-class MenuItem(ExplorerMenuItem):
+class DrawerMenuItem(ExplorerMenuItem):
     template = "wagtail_drawer/explorer_menu_item.html"
+
+    @property	
+    def media(self):	
+        return forms.Media(js=[static('wagtail_drawer/js/drawer-menu.js')])
---
+++ wagtail_drawer_test_app/home/wagtail_hooks.py
@@ -0,0 +1,15 @@
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+from wagtail.core import hooks
+
+from wagtail_drawer.menu import DrawerMenuItem
+from wagtail.admin.wagtail_hooks import ExplorerMenuItem
+
+
+@hooks.register('register_admin_menu_item')
+def register_explorer_menu_item():
+    return DrawerMenuItem(
+        _('Drawer'), reverse('wagtailadmin_explore_root'),
+        name='explorer',
+        classnames='icon icon-folder-open-inverse',
+        order=10)
