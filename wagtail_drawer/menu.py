48aab68dc7a2553314a9f11cbadc6f6ca260d966
rinti <andreas.bernacca@frojd.se>
2019-04-30 09:37:15 +0200

    Remove unused imports and create template file

--- wagtail_drawer/menu.py
+++ wagtail_drawer/menu.py
@@ -1,6 +1,4 @@
 from wagtail.admin.wagtail_hooks import ExplorerMenuItem
-from wagtail.admin.utils import user_has_any_page_permission
-from wagtail.admin.navigation import get_explorable_root_page
 
 
 class MenuItem(ExplorerMenuItem):
