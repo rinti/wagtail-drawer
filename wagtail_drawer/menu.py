4e21dcb185d52219ed2520bbb16c24d5a6f70fb7
rinti <andreas.bernacca@frojd.se>
2019-04-30 09:26:03 +0200

    Apply black formatting

--- wagtail_drawer/menu.py
+++ wagtail_drawer/menu.py
@@ -2,5 +2,6 @@ from wagtail.admin.wagtail_hooks import ExplorerMenuItem
 from wagtail.admin.utils import user_has_any_page_permission
 from wagtail.admin.navigation import get_explorable_root_page
 
+
 class MenuItem(ExplorerMenuItem):
-    template = 'wagtail_drawer/explorer_menu_item.html'
+    template = "wagtail_drawer/explorer_menu_item.html"
