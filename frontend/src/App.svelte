76bc1ec8db3b4298c1303bbba754173ea7d45e20
rinti <andreas.bernacca@frojd.se>
2019-04-30 21:52:55 +0200

    First working prototype of drawer behaviour

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,19 +1,17 @@
 <script>
-    export let data = {items: []};
-    export let mock = true;
+    import Item from './Item.svelte';
+
+    let items = [];
 
     const fetchInitial = async () => {
         const response = await fetch('http://localhost:8000/admin/api/v2beta/pages/?child_of=1&for_explorer=1')
-        data = await response.json()
-        console.log(data)
+        const data = await response.json()
+        items = data.items
     }
 
-    const fetchChildren = async (e, item) => {
-        e.preventDefault()
-        const url = item.meta.children.listing_url.replace('localhost', 'localhost:8000')
-
-        const response = await fetch(url)
-        data = await response.json()
+    const topLevelItems = () => {
+        const x = items.filter(item => item.parent === undefined)
+        return x
     }
 
     fetchInitial()
@@ -23,20 +21,7 @@
 </style>
 
 <ul>
-{#each data.items as item}
-    <li>
-        <a href="#" on:click={(e) => fetchChildren(e, item)}>
-            {#if item.meta.children}&gt;{/if} {item.title}
-        </a>
-        {#if item.items}
-            <ul>
-                {#each item.items as child}
-                    <a href="" on:click={(e) => fetchChildren(e, child)}>
-                        {#if child.meta.children}&gt;{/if} {child.title}
-                    </a>
-                {/each}
-            </ul>
-        {/if}
-    </li>
-{/each}
+    {#each topLevelItems(items) as item}
+        <Item {item} />
+    {/each}
 </ul>
---
+++ frontend/src/Item.svelte
@@ -0,0 +1,30 @@
+<script>
+    let children = [];
+    export let item;
+
+    const fetchChildren = async () => {
+        const url = item.meta.children.listing_url.replace('localhost', 'localhost:8000')
+        const response = await fetch(`${url}&for_explorer=1`)
+
+        const data = await response.json()
+        children = data.items
+    }
+
+    $: hasChildren = item.meta.children && item.meta.children.count > 0;
+</script>
+
+<style>
+</style>
+
+<li>
+    <a href="#" on:click={(e) => fetchChildren(item)}>
+        {#if hasChildren}&gt;{/if} {item.title}
+    </a>
+    {#if children.length > 0}
+        <ul>
+            {#each children as child}
+                <svelte:self item={child} />
+            {/each}
+        </ul>
+    {/if}
+</li>
