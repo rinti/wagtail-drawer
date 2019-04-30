0fcbc0877098e6483a6116c6385e87b7901ccce0
rinti <andreas.bernacca@frojd.se>
2019-04-30 10:27:05 +0200

    Fix imports

--- wagtail_drawer/menu.py
+++ wagtail_drawer/menu.py
@@ -1,9 +1,11 @@
+from django import forms
+from django.contrib.staticfiles.templatetags.staticfiles import static
 from wagtail.admin.wagtail_hooks import ExplorerMenuItem
 
 
 class DrawerMenuItem(ExplorerMenuItem):
     template = "wagtail_drawer/explorer_menu_item.html"
 
-    @property	
-    def media(self):	
+    @property
+    def media(self):
         return forms.Media(js=[static('wagtail_drawer/js/drawer-menu.js')])
